b8019cbc74f1478320c9bfa72c232baa7b05a667
fmihel <fmihel@gmail.com>
2019-04-26 14:02:30 +0300

    v1.1.0/260419_1

--- wsi.php
+++ wsi.php
@@ -4,8 +4,8 @@ header('Cache-Control: no-cache, no-store, must-revalidate'); // HTTP 1.1.
 header('Pragma: no-cache'); // HTTP 1.0.
 header('Expires: 0'); // Proxies.
 
-define('WSI_DEV_VERSION','260319_1');
-define('WSI_GIT_VERSION','1.0.5');
+define('WSI_DEV_VERSION','260419_1');
+define('WSI_GIT_VERSION','1.1.0');
 
 error_reporting(0);
 /*
